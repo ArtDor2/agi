1f9d894219a6cdbe8ee3e64e9ec75b5dd63fdec9
Artur Dorovskikh <ArturDorovskikh@users.noreply.github.com>
2018-01-15 21:09:16 -0600

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,17 @@
+# 2018-01-15 mon
+# basic agi generator
+
+file = open("text_nano.txt", "r")
+text = file.read()  # text env for now
+file.close()
+
+def node_add(node_name):
+    # node add to mind
+
+
+
+# read the env_text
+for pos in range(len(text)):
+    # pattern match 1 neighbors left and right
+    if str(text[pos]):
+        #node_add(pos)
